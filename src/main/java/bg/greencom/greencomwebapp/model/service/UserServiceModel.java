5bbe3704fd4b791dd818cfa254e326ee5174d807
Veselin Hristov <v.h.hristov7@gmail.com>
2023-02-09 13:16:08 +0200

    added modelmapper dependancy

--- src/main/java/bg/greencom/greencomwebapp/model/service/UserServiceModel.java
+++ src/main/java/bg/greencom/greencomwebapp/model/service/UserServiceModel.java
@@ -1,4 +1,55 @@
 package bg.greencom.greencomwebapp.model.service;
 
 public class UserServiceModel {
+
+    private String firstName;
+    private String lastname;
+    private String username;
+    private String email;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserServiceModel setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public UserServiceModel setLastname(String lastname) {
+        this.lastname = lastname;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserServiceModel setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserServiceModel setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserServiceModel setPassword(String password) {
+        this.password = password;
+        return this;
+    }
 }
