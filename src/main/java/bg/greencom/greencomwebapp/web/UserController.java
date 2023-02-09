ca1567bec1762aecc7681b6e07430ffe9dae0fc2
Veselin Hristov <v.h.hristov7@gmail.com>
2023-02-09 10:47:03 +0200

    created login page

--- src/main/java/bg/greencom/greencomwebapp/web/UserController.java
+++ src/main/java/bg/greencom/greencomwebapp/web/UserController.java
@@ -1,7 +1,16 @@
 package bg.greencom.greencomwebapp.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
 }
