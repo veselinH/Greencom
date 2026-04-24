3f51c024de072d26a1ccfe5cb6ed1ca3ac689ac8
veselinH <v.h.hristov7@gmail.com>
2026-04-24 17:07:58 +0300

    added method to search plan by name

--- src/main/java/bg/greencom/greencomwebapp/repository/InternetPlanRepository.java
+++ src/main/java/bg/greencom/greencomwebapp/repository/InternetPlanRepository.java
@@ -6,15 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface InternetPlanRepository extends JpaRepository<InternetPlanEntity, Long> {
-//    @Query("SELECT i FROM InternetPlanEntity i ORDER BY i.price")
-//    List<InternetPlanEntity> findAllInternetPlansOrderedByPrice();
+
       @Query("SELECT i FROM InternetPlanEntity i " +
              "JOIN FETCH i.internetType " +
              "LEFT JOIN FETCH i.internetExtras " +
               "ORDER BY i.price")
       List<InternetPlanEntity> findAllInternetPlansOrderedByPrice();
 
+      Optional<InternetPlanEntity> findByName(String name);
 }
