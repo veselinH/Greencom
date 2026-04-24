75e7fd53847ed9beba485ff713f11831476f6033
veselinH <v.h.hristov7@gmail.com>
2026-04-24 19:00:15 +0300

    implemented getValue() method

--- src/main/java/bg/greencom/greencomwebapp/model/entity/enums/TelevisionTypeEnum.java
+++ src/main/java/bg/greencom/greencomwebapp/model/entity/enums/TelevisionTypeEnum.java
@@ -1,5 +1,12 @@
 package bg.greencom.greencomwebapp.model.entity.enums;
 
 public enum TelevisionTypeEnum {
-    SATELLITE, INTERACTIVE
+    SATELLITE("Satellite"),
+    INTERACTIVE("Interactive");
+
+    private final String value;
+
+    TelevisionTypeEnum(String value) {this.value = value;}
+
+    public String getValue() {return value;}
 }
