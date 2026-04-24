27def19c1534e2b1ea460696ffdbcb99357ca3db
veselinH <v.h.hristov7@gmail.com>
2026-04-24 19:00:30 +0300

    new method to fetch all television plans ordered by price

--- src/main/java/bg/greencom/greencomwebapp/repository/TelevisionPlanRepository.java
+++ src/main/java/bg/greencom/greencomwebapp/repository/TelevisionPlanRepository.java
@@ -2,8 +2,17 @@ package bg.greencom.greencomwebapp.repository;
 
 import bg.greencom.greencomwebapp.model.entity.TelevisionPlanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface TelevisionPlanRepository extends JpaRepository<TelevisionPlanEntity, Long> {
+
+    @Query("SELECT t FROM TelevisionPlanEntity t " +
+            "JOIN FETCH t.televisionType " +
+            "LEFT JOIN FETCH t.additionalPackages " +
+            "ORDER BY t.price")
+    Set<TelevisionPlanEntity> findAllTelevisionPlansOrderedByPrice();
 }
