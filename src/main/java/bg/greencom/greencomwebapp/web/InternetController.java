c92db3fd5c273b6c006e15b6c194309f09158b34
veselinH <v.h.hristov7@gmail.com>
2026-04-24 17:07:42 +0300

    added sign option for internet plan

--- src/main/java/bg/greencom/greencomwebapp/web/InternetController.java
+++ src/main/java/bg/greencom/greencomwebapp/web/InternetController.java
@@ -2,12 +2,16 @@ package bg.greencom.greencomwebapp.web;
 
 import bg.greencom.greencomwebapp.model.binding.InternetPlanBindingModel;
 import bg.greencom.greencomwebapp.model.service.InternetPlanServiceModel;
+import bg.greencom.greencomwebapp.model.user.GreencomUserDetails;
 import bg.greencom.greencomwebapp.model.view.InternetPlanViewModel;
+import bg.greencom.greencomwebapp.model.view.VoicePlanViewModel;
 import bg.greencom.greencomwebapp.service.InternetPlanService;
+import bg.greencom.greencomwebapp.service.UserService;
 import bg.greencom.greencomwebapp.validation.group.onCreate;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
@@ -15,16 +19,20 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Base64;
+
 @Controller
 @RequestMapping("/internet")
 public class InternetController {
 
     private final InternetPlanService internetPlanService;
     private final ModelMapper modelMapper;
+    private final UserService userService;
 
-    public InternetController(InternetPlanService internetPlanService, ModelMapper modelMapper) {
+    public InternetController(InternetPlanService internetPlanService, ModelMapper modelMapper, UserService userService) {
         this.internetPlanService = internetPlanService;
         this.modelMapper = modelMapper;
+        this.userService = userService;
     }
 
     @ModelAttribute
@@ -94,6 +102,32 @@ public class InternetController {
         }
 
         internetPlanService.updateInternetPlan(modelMapper.map(internetPlanBindingModel, InternetPlanServiceModel.class));
+
+        return "redirect:/internet/internet-plans";
+    }
+
+    @GetMapping("/internet-plan/{id}")
+    public String signInternetPlan(@PathVariable Long id, Model model){
+        InternetPlanViewModel internetPlanViewModel = internetPlanService.findById(id);
+
+        model.addAttribute("internetPlanFromRepo", internetPlanViewModel);
+
+        return "internet-plans/internet-plans";
+    }
+
+//    Sign a contract to the user
+    @PatchMapping("/internet-plan/{id}")
+    public String signInternetPlanConfirm(@PathVariable Long id,
+                                          @AuthenticationPrincipal GreencomUserDetails userDetails,
+                                          @RequestParam String signature) {
+
+//        Decode the signature image
+        String base64Data = signature.split(",")[1];
+        byte[] signSignature = Base64.getDecoder().decode(base64Data);
+//        Retrieve the internet plan
+        InternetPlanViewModel internetPlan = internetPlanService.findById(id);
+
+        userService.signInternetPlan(internetPlan, userDetails, signSignature);
 
         return "redirect:/internet/internet-plans";
     }
