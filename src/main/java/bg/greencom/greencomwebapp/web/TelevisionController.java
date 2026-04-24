b40723ca769a0ae971e958d5968e78718927ecaf
veselinH <v.h.hristov7@gmail.com>
2026-04-24 18:59:59 +0300

    new Get request for all television plans and Get request for adding a new television plan

--- src/main/java/bg/greencom/greencomwebapp/web/TelevisionController.java
+++ src/main/java/bg/greencom/greencomwebapp/web/TelevisionController.java
@@ -1,15 +1,47 @@
 package bg.greencom.greencomwebapp.web;
 
+import bg.greencom.greencomwebapp.model.binding.TelevisionPlanBindingModel;
+import bg.greencom.greencomwebapp.service.TelevisionPlanService;
+import bg.greencom.greencomwebapp.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/television")
 public class TelevisionController {
 
-    @GetMapping("/television-plans")
-    public String televisionPlans(){
-        return "television-plans";
+    private final TelevisionPlanService televisionPlanService;
+    private final ModelMapper modelMapper;
+    private final UserService userService;
+
+    public TelevisionController(TelevisionPlanService televisionPlanService, ModelMapper modelMapper, UserService userService) {
+        this.televisionPlanService = televisionPlanService;
+        this.modelMapper = modelMapper;
+        this.userService = userService;
     }
+
+    @ModelAttribute
+    public TelevisionPlanBindingModel televisionPlanBindingModel(){
+        return new TelevisionPlanBindingModel();
+    }
+
+    @GetMapping("/television-plans")
+    public String televisionPlans(Model model){
+
+        model.addAttribute("allTelevisionPlans", televisionPlanService.findAllPlansOrderedByPrice());
+
+        return "television-plans/television-plans";
+    }
+
+    @GetMapping("/add-television-plan")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String addTelevisionPlan() {return "television-plans/add-television-plan";}
+
+
+
 }
