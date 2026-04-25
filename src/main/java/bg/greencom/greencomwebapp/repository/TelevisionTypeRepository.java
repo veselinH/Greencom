b2386b1bc2ef32abdac7674e5306150a9f617356
veselinH <v.h.hristov7@gmail.com>
2026-04-25 17:37:29 +0300

    new method findByName

--- src/main/java/bg/greencom/greencomwebapp/repository/TelevisionTypeRepository.java
+++ src/main/java/bg/greencom/greencomwebapp/repository/TelevisionTypeRepository.java
@@ -1,9 +1,11 @@
 package bg.greencom.greencomwebapp.repository;
 
 import bg.greencom.greencomwebapp.model.entity.TelevisionTypeEntity;
+import bg.greencom.greencomwebapp.model.entity.enums.TelevisionTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TelevisionTypeRepository extends JpaRepository<TelevisionTypeEntity, Long> {
+    TelevisionTypeEntity findByName(TelevisionTypeEnum televisionType);
 }
