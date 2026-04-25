55b4f201a6bc9fde26df8e297092161100557744
veselinH <v.h.hristov7@gmail.com>
2026-04-25 17:34:06 +0300

    new method findAllByOrderByNameAsc

--- src/main/java/bg/greencom/greencomwebapp/repository/AdditionalPackageRepository.java
+++ src/main/java/bg/greencom/greencomwebapp/repository/AdditionalPackageRepository.java
@@ -4,6 +4,9 @@ import bg.greencom.greencomwebapp.model.entity.AdditionalPackageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AdditionalPackageRepository extends JpaRepository<AdditionalPackageEntity, Long> {
+    List<AdditionalPackageEntity> findAllByOrderByNameAsc();
 }
