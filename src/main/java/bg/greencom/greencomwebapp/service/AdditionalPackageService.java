712fe3bead8fc59af262772f78939e30083fbafa
veselinH <v.h.hristov7@gmail.com>
2026-04-25 17:34:20 +0300

    new methods

--- src/main/java/bg/greencom/greencomwebapp/service/AdditionalPackageService.java
+++ src/main/java/bg/greencom/greencomwebapp/service/AdditionalPackageService.java
@@ -1,5 +1,17 @@
 package bg.greencom.greencomwebapp.service;
 
+import bg.greencom.greencomwebapp.model.entity.AdditionalPackageEntity;
+import bg.greencom.greencomwebapp.model.view.AdditionalPackageViewModel;
+import bg.greencom.greencomwebapp.repository.AdditionalPackageRepository;
+
+import java.util.List;
+import java.util.Set;
+
+
 public interface AdditionalPackageService {
     void initialize();
+
+    List<AdditionalPackageViewModel> findAllOrderedByName();
+
+    Set<AdditionalPackageEntity> findAllByIds(Set<Long> additionalPackageIds);
 }
