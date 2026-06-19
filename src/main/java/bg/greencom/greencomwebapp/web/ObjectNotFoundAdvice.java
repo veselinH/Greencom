108cf835abd412b554a5523638afff35f43636ab
veselinH <v.h.hristov7@gmail.com>
2026-06-19 11:43:31 +0300

    added comment and renamed

--- src/main/java/bg/greencom/greencomwebapp/web/ObjectNotFoundAdvice.java
+++ src/main/java/bg/greencom/greencomwebapp/web/ObjectNotFoundAdvice.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * Global handler for {@link ObjectNotFoundException}.
+ * Renders the custom object-not-found error page with the missing entity's type and ID
+ * instead of the generic 404, so users see a more descriptive message.
+ */
 @ControllerAdvice
 public class ObjectNotFoundAdvice {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ModelAndView onPlanNotFound(ObjectNotFoundException exception) {
+    public ModelAndView onObjectNotFound(ObjectNotFoundException exception) {
 
         ModelAndView modelAndView = new ModelAndView("error/custom-errors/object-not-found");
         modelAndView.addObject("objectId", exception.getObjectId());
