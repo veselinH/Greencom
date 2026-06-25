2abb6e6570988746e07cf82c3170713f0425ad4f
veselinH <v.h.hristov7@gmail.com>
2026-06-25 23:50:03 +0300

    added new role MODERATOR new method for value

--- src/main/java/bg/greencom/greencomwebapp/model/entity/enums/UserRoleEnum.java
+++ src/main/java/bg/greencom/greencomwebapp/model/entity/enums/UserRoleEnum.java
@@ -1,5 +1,17 @@
 package bg.greencom.greencomwebapp.model.entity.enums;
 
 public enum UserRoleEnum {
-    ADMIN, USER
+    ADMIN("Admin"),
+    USER("User"),
+    MODERATOR("Moderator");
+
+    private final String value;
+
+    UserRoleEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
